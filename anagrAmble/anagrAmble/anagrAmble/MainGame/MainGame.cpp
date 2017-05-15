879e29b740465061bc29f5f5e6a45686ee8a77ee
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-05-15 15:46:55 +0900

    ファイルをアップロード

---
+++ anagrAmble/anagrAmble/anagrAmble/MainGame/MainGame.cpp
@@ -0,0 +1,55 @@
+﻿//==================================================================================================================================//
+//!< @file		MainGame.cpp
+//!< @brief		MainGameクラス実装
+//!< @author	T.Haga
+//==================================================================================================================================//
+
+/* Includes --------------------------------------------------------------------------------------------------- */
+
+#include "MainGame.h"
+#include "../SharokuLibrary/sl/sl.h"
+
+namespace ar
+{
+
+/* Public Functions ------------------------------------------------------------------------------------------- */
+
+MainGame::MainGame(void)
+	: m_pLibrary(nullptr)
+{
+	sl::ISharokuLibrary::Create();
+	m_pLibrary = sl::ISharokuLibrary::Instance();
+
+	// ライブラリの初期化
+	sl::t_char*	windowTitle		= "anagrAmble";			// ウィンドウのタイトル
+	int			windowWidth		= 1920;					// ウィンドウの横幅
+	int			windowHeight	= 1080;					// ウィンドウの縦幅
+	m_pLibrary->Initialize(windowTitle, windowWidth, windowHeight);
+}
+
+MainGame::~MainGame(void)
+{
+	sl::ISharokuLibrary::Delete();
+}
+
+void MainGame::Roop(void)
+{
+	while(true)
+	{
+		if(m_pLibrary->UpdateWindow())
+		{
+			break;
+		}
+		else
+		{
+
+		}
+	}
+}
+
+} // namespace ar
+
+//==================================================================================================================================//
+// END OF FILE
+//==================================================================================================================================//
+
