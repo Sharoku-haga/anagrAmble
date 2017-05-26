e0304ee75a73639d748c9ad7c7f7ae838100b2b4
SharokuHaga-PC\Sharoku Haga <holmes221nob@gmail.com>
2017-05-27 01:21:29 +0900

    [enh #26] BasePointクラス実装

--- anagrAmble/anagrAmble/anagrAmble/MainGame/SceneManager/Scene/GameScene/StageManager/Stage/BasePoint.h
+++ anagrAmble/anagrAmble/anagrAmble/MainGame/SceneManager/Scene/GameScene/StageManager/Stage/BasePoint.h
@@ -7,22 +7,70 @@
 #ifndef AR_BASE_POINT_H
 #define AR_BASE_POINT_H
 
+#include "../SharokuLibrary/sl/sl.h"
+
 namespace ar
 {
 
+class EventLisner;
+
 //======================================================================//
-//!< ゲーム画面の描画ポイントになるクラス
+//!< ベースポイントクラス
+//!< 表示する画面範囲の基準となるポイントのこと
 //======================================================================//
 class BasePoint
 {
 
 public:
 	/** Constructor */
-	BasePoint();
+	BasePoint(void);
 
 	/** Destructor */
-	~BasePoint();
+	~BasePoint(void);
 
+	/** 
+	* 初期化関数. ステージ生成時に呼ぶ関数
+	* @param[in] stageWidth ステージの横の長さ
+	* @param[in] rPlayerPos	プレイヤーの位置座標(ステージ生成時の初期座標)
+	*/
+	void Initialize(float stageWidth, const sl::SLVECTOR2& rPlayerPos);
+
+	/** 
+	* 更新関数
+	* 毎フレームよばれる関数. 
+	*/
+	void Update(void);
+
+	/** 
+	* Getter. BasePointの位置座標を取得する関数 
+	* @return BasePointの位置座標
+	*/
+	inline const sl::SLVECTOR2& GetPos(void) const{ return m_Pos; }
+
+	/**
+	* Setter プレイヤーの現在の位置座標をセットする関数
+	* @param[in] rCurrentPlayerPos プレイヤーの位置座標
+	*/
+	inline void SetCuurentPlayerPos(const sl::SLVECTOR2& rCuurentPlayerPos) { m_CuurentPlayerPos = rCuurentPlayerPos; }
+
+private:
+	sl::SLVECTOR2				m_Pos;						//!< 位置座標
+	sl::SLVECTOR2				m_CuurentPlayerPos;			//!< 現在のプレイヤー座標
+	sl::SLVECTOR2				m_OldPlayerPos;				//!< 以前のプレイヤー座標
+	float						m_StageWidth;				//!< ステージ全体の横の長さ
+	EventLisner*				m_pEventLisner;				//!< EventLisnerクラスのインスタンスへのポインタ
+
+	SL_DISALLOW_COPY_AND_ASSIGN(BasePoint);					//!< コピー禁止マクロ
+
+	/** イベント処理を行う関数 */
+	void HandleEvent(void);
+
+	/** 
+	* BasePointの移動を制御する関数
+	* プレイヤーが動いたというイベントが発生したときに呼ばれる
+	*/
+	void Move(void);
+	
 };	// class BasePoint
 
 }	// namespace ar
